2b8b3662ab900925cb4b8f545d3899232630616b
Allen Moody <allen3kgt@gmail.com>
2016-10-26 21:05:07 -0400

    Step 2 completed

--- code2040.py
+++ code2040.py
@@ -1,9 +1,24 @@
+#   CODE 2040 API Challenge
+#   
+#   Author: AllenDM Allen Moody
+
 import requests
 
-token = input("Enter API Token")
 
+token = input("Enter API Token: ")
+
+# Step 1
 register = {'token' : token, 'github': 'https://github.com/AllenDM/apichallenge'}
 
+requests.post("http://challenge.code2040.org/api/register", data = register)
 
-r = requests.post("http://challenge.code2040.org/api/register", data = register)
+# Step 2
+reverseRegister = {'token': token}
 
+r = requests.post("http://challenge.code2040.org/api/reverse", data = reverseRegister)
+
+reversedString = r.text[::-1]
+
+reverseSolution = {'token': token, 'string': reversedString}
+
+requests.post("http://challenge.code2040.org/api/reverse/validate", data = reverseSolution)
