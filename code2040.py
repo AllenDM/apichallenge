1884233073d274287a5ef338581597f55a1b2bd3
Allen Moody <allen3kgt@gmail.com>
2016-10-27 00:55:24 -0400

    Step 4 completed

--- code2040.py
+++ code2040.py
@@ -1,25 +1,33 @@
-#   CODE 2040 API Challenge
-#   
+#   CODE 2040 API Challenge 
 #   Author: AllenDM Allen Moody
 import ast
 import requests
 
-token = input("Enter API Token: ")
-
 # Step 1
-register = {'token' : token, 'github': 'https://github.com/AllenDM/apichallenge'}
-requests.post("http://challenge.code2040.org/api/register", data = register)
+token = input("Enter API Token: ")
+requests.post("http://challenge.code2040.org/api/register", data = {'token' : token, 'github': 'https://github.com/AllenDM/apichallenge'})
 
 # Step 2
 plainToken = {'token': token}
-r = requests.post("http://challenge.code2040.org/api/reverse", data = plainToken)
-reversedString = r.text[::-1]
-reverseSolution = {'token': token, 'string': reversedString}
-requests.post("http://challenge.code2040.org/api/reverse/validate", data = reverseSolution)
+reverseData = requests.post("http://challenge.code2040.org/api/reverse", data = plainToken)
+reversedString = reverseData.text[::-1]
+reverseSubmit = {'token':token, 'string': reversedString}
+requests.post("http://challenge.code2040.org/api/reverse/validate", data = reverseSubmit)
 
 # Step 3
-d = requests.post("http://challenge.code2040.org/api/haystack", data = plainToken)
-needleMap = ast.literal_eval(d.text)
+needleData = requests.post("http://challenge.code2040.org/api/haystack", data = plainToken)
+needleMap = ast.literal_eval(needleData.text)
 needleIndex = (needleMap['haystack'].index(needleMap['needle']))
-needleSolution = {'token': token, 'needle': needleIndex}
-requests.post("http://challenge.code2040.org/api/haystack/validate", data = needleSolution)
+needleSubmit = {'token':token, 'needle':needleIndex}
+requests.post("http://challenge.code2040.org/api/haystack/validate", data = needleSubmit)
+
+# Step 4
+prefixData = requests.post("http://challenge.code2040.org/api/prefix", data = plainToken)
+prefixMap = ast.literal_eval(prefixData.text)
+prefixLength = len(prefixMap['prefix'])
+solution = []
+for x in prefixMap['array']:
+	if x[:prefixLength] != prefixMap['prefix']:
+	    solution.append(x)
+prefixSubmit = {'token': token, 'array': solution}	    
+requests.post("http://challenge.code2040.org/api/prefix/validate", json = prefixSubmit)
