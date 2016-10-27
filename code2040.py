6ffb0b0319a694e183efd189ffdf8e7a937bfe83
Allen Moody <allen3kgt@gmail.com>
2016-10-26 21:51:52 -0400

    Step 3 completed

--- code2040.py
+++ code2040.py
@@ -1,24 +1,25 @@
 #   CODE 2040 API Challenge
 #   
 #   Author: AllenDM Allen Moody
-
+import ast
 import requests
-
 
 token = input("Enter API Token: ")
 
 # Step 1
 register = {'token' : token, 'github': 'https://github.com/AllenDM/apichallenge'}
-
 requests.post("http://challenge.code2040.org/api/register", data = register)
 
 # Step 2
-reverseRegister = {'token': token}
-
-r = requests.post("http://challenge.code2040.org/api/reverse", data = reverseRegister)
-
+plainToken = {'token': token}
+r = requests.post("http://challenge.code2040.org/api/reverse", data = plainToken)
 reversedString = r.text[::-1]
-
 reverseSolution = {'token': token, 'string': reversedString}
-
 requests.post("http://challenge.code2040.org/api/reverse/validate", data = reverseSolution)
+
+# Step 3
+d = requests.post("http://challenge.code2040.org/api/haystack", data = plainToken)
+needleMap = ast.literal_eval(d.text)
+needleIndex = (needleMap['haystack'].index(needleMap['needle']))
+needleSolution = {'token': token, 'needle': needleIndex}
+requests.post("http://challenge.code2040.org/api/haystack/validate", data = needleSolution)
