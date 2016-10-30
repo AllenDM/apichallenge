43f31e341e44fa0c5730fbeb4db7eacb51a85d0f
Allen Moody <allen3kgt@gmail.com>
2016-10-30 00:52:14 -0400

    refactoring and adding file input

--- code2040.py
+++ code2040.py
@@ -6,43 +6,44 @@ import dateutil.parser
 import dateutil.relativedelta
 
 # Step 1
-token = input("Enter API Token: ")
+newFile = open('token.txt')
+token = newFile.read()
+token = token.rstrip()
 register = {'token': token, 'github': 'https://github.com/AllenDM/apichallenge'}
-requests.post("http://challenge.code2040.org/api/register", data = register)
+requests.post("http://challenge.code2040.org/api/register", data=register)
 
 # Step 2
 plainToken = {'token': token}
-reverseData = requests.post("http://challenge.code2040.org/api/reverse", data = plainToken)
+reverseData = requests.post("http://challenge.code2040.org/api/reverse", data=plainToken)
 reversedString = reverseData.text[::-1]
 reverseSubmit = {'token':token, 'string': reversedString}
-requests.post("http://challenge.code2040.org/api/reverse/validate", data = reverseSubmit)
+requests.post("http://challenge.code2040.org/api/reverse/validate", data=reverseSubmit)
 
 # Step 3
-needleData = requests.post("http://challenge.code2040.org/api/haystack", data = plainToken)
+needleData = requests.post("http://challenge.code2040.org/api/haystack", data=plainToken)
 needleMap = ast.literal_eval(needleData.text)
 needleIndex = (needleMap['haystack'].index(needleMap['needle']))
 needleSubmit = {'token':token, 'needle':needleIndex}
-requests.post("http://challenge.code2040.org/api/haystack/validate", data = needleSubmit)
+requests.post("http://challenge.code2040.org/api/haystack/validate", data=needleSubmit)
 
 # Step 4
-prefixData = requests.post("http://challenge.code2040.org/api/prefix", data = plainToken)
+prefixData = requests.post("http://challenge.code2040.org/api/prefix", data=plainToken)
 prefixMap = ast.literal_eval(prefixData.text)
-prefixLength = len(prefixMap['prefix'])
 solution = []
-for x in prefixMap['array']:
-	if x[:prefixLength] != prefixMap['prefix']:
-	    solution.append(x)
+for word in prefixMap['array']:
+	if not word.startswith(prefixMap['prefix']):
+	    solution.append(word)
 prefixSubmit = {'token': token, 'array': solution}	    
-requests.post("http://challenge.code2040.org/api/prefix/validate", json = prefixSubmit)
+requests.post("http://challenge.code2040.org/api/prefix/validate", json=prefixSubmit)
 
-#Step 5
-dateData = requests.post("http://challenge.code2040.org/api/dating", data = plainToken)
+# Step 5
+dateData = requests.post("http://challenge.code2040.org/api/dating", data=plainToken)
 dateMap = ast.literal_eval(dateData.text)
 dateStamp = dateutil.parser.parse(dateMap['datestamp'])
-newDate = dateStamp+dateutil.relativedelta.relativedelta(seconds=+dateMap['interval'])
+newDate = dateStamp + dateutil.relativedelta.relativedelta(seconds =+ dateMap['interval'])
 newDate = newDate.isoformat()
 newDateString = str(newDate)
 if newDateString[19:] == "+00:00":
 	newDateString = newDateString[:19] + "Z"
 dateSolution = {'token': token, 'datestamp': newDateString}
-requests.post("http://challenge.code2040.org/api/dating/validate", json = dateSolution)
+requests.post("http://challenge.code2040.org/api/dating/validate", json=dateSolution)
